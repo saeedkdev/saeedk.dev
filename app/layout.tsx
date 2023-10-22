652402d17be6b096e318bbcfcf00c3d8f4829988
Saeed Karimi <saeedsektor@gmail.com>
2023-10-22 10:18:48 -0700

    added social links

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter, Fira_Sans } from 'next/font/google'
 import './globals.css'
 import Link from 'next/link'
 
+import { Github, Youtube } from 'lucide-react'
+
 const inter = Inter({ subsets: ['latin'] })
 const firaSans = Fira_Sans({ subsets: ['latin'], weight: ['400', '700']})
 
@@ -21,7 +23,18 @@ export default function RootLayout({
         <body className={firaSans.className}>
         <main className="flex flex-col w-full items-center">
             <section className="flex flex-col w-[900px]">
-                <nav className="flex flex-row justify-between w-[240px] max-w-2xl py-4 mt-[100px]">
+                <div className="flex flex-row justify-between mt-[100px] w-full">
+                    <Link href="https://github.com/saeedkdev" target='_blank' className='flex flex-row gap-1 rounded-md p-2 border 
+                                hover:border-gray-900
+                                hover:bg-gray-900 hover:text-white transition' >
+                        <Github size={24}/> My Work
+                    </Link>
+                    <Link href="https://www.youtube.com/@skdev11" target='_blank' className='flex flex-row gap-1 p-2 hover:text-red-500' >
+                        <Youtube size={24}/> My Life as a software engineer
+                    </Link>
+                </div>
+                <hr className="w-full mt-5"/>
+                <nav className="flex flex-row justify-between w-[240px] max-w-2xl py-4">
                     <Link href="/">
                         home
                     </Link>
